e5acd0e50cb7832d06d3aab7242b36f44dbcf1d5
yourangrang <163999418+yourangrang@users.noreply.github.com>
2024-08-27 15:43:41 +0900

    추천 유투버 작업

--- src/components/contents/Developer.jsx
+++ src/components/contents/Developer.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
+import { developerText } from '../../data/developer';
+import { Link } from 'react-router-dom';
+
 const Developer = () => {
-  return (
-    <div>Developer</div>
-  )
+    return (
+        <section id='developer'>
+            <h2>😎 추천 유투버를 소개합니다.</h2>
+            <div className="developer__inner overflow">
+                {developerText.map((developer, key) => (
+                    <div className="developer" key={key}>
+                        <div className="developer__img play__icon">
+                            <Link to={`/channel/${developer.channelId}`}>
+                                <img src={developer.img} alt={developer.name} />
+                            </Link>
+                        </div>
+                        <div className="developer__info">
+                            <Link to={`/channel/${developer.channelId}`}>
+                                {developer.name}
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
 }
 
 export default Developer
