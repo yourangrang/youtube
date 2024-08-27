d8e1920e71ba51f07b44af5d723069d847593511
yourangrang <163999418+yourangrang@users.noreply.github.com>
2024-08-27 16:44:58 +0900

    메인섹션 나머지콘텐츠 작업

--- src/components/contents/Gsap.jsx
+++ src/components/contents/Gsap.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import { gsapText } from '../../data/gsap'
+import { Link } from 'react-router-dom'
+
+
 const Gsap = () => {
   return (
-    <div>Gsap</div>
+      <section id='webd'>
+            <h2>🤓 GSAP 패럴랙스 효과를 하고 싶다면!</h2>
+            <div className="video__inner">
+                {gsapText.map((video, key)=>(
+                    <div className="video" key={key}>
+                        <div className="video__thumb play__icon">
+                            <Link to={'/video/video.videoId'}>
+                                <img src={video.img} alt={video.title} />
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
   )
 }
 
--- src/components/contents/Youtube.jsx
+++ src/components/contents/Youtube.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import { youtubeText } from '../../data/youtube'
+import { Link } from 'react-router-dom'
+
+
 const Youtube = () => {
   return (
-    <div>Youtube</div>
+    <section id='youtube'>
+    <h2>🤗 포트폴리오 만드는 방법을 공유합니다.</h2>
+    <div className="video__inner">
+        {youtubeText.map((video, key)=>(
+            <div className="video" key={key}>
+                <div className="video__thumb play__icon">
+                    <Link to={'/video/video.videoId'}>
+                        <img src={video.img} alt={video.title} />
+                    </Link>
+                </div>
+            </div>
+        ))}
+    </div>
+</section>
   )
 }
 
